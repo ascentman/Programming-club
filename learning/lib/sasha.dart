5d018f2bf03564993189a52713b59f07f622cb23
zEML9K <oleksandrmelnik852@gmail.com>
2023-03-11 15:31:01 +0200

    Create sasha.dart

---
+++ learning/lib/sasha.dart
@@ -0,0 +1,34 @@
+class Car {
+    final int weight;
+    final String color;
+
+    void drive (){
+        print ("Ми їдемо на машині");
+    }
+
+    Car({
+        required this.weight,
+        required this.color,
+    });
+}
+
+class BMWCar extends Car {
+    final bool isOld;
+
+    BMWCar({
+        required super.weight,
+        required super.color,
+        required this.isOld,
+});
+}
+
+class MercedesCar extends Car {
+    final int seatsCount;
+
+
+MercedesCar({
+    required super.weight,
+    required super.color,
+    required this. seatsCount, required bool isOld,
+});
+}
