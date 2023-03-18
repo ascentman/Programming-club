6863ef473386b9b768378ebc4d129b72e16d8552
Volodymyr Rykhva <ascentman@ukr.net>
2023-03-18 15:24:51 +0200

    Update main.dart

--- learning/lib/main.dart
+++ learning/lib/main.dart
@@ -18,85 +18,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool isOnStart = true;
-  final myBMW = Car(imagePath: 'assets/bmw.png', maxSpeed: 200);
-  final myMercedes = Car(imagePath: 'assets/mercedes.png', maxSpeed: 300);
-
-  void _incrementCounter() {
-    setState(() {
-      isOnStart = !isOnStart;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CarWidget(
-              car: myBMW,
-              isOnStart: isOnStart,
-            ),
-            CarWidget(
-              car: myMercedes,
-              isOnStart: isOnStart,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.local_fire_department),
-      ),
-    );
-  }
-}
-
-class Car {
-  final String imagePath;
-  final int maxSpeed;
-
-  Car({
-    required this.imagePath,
-    required this.maxSpeed,
-  });
-}
-
-class CarWidget extends StatelessWidget {
-  final Car car;
-  final bool isOnStart;
-
-  const CarWidget({
-    Key? key,
-    required this.car,
-    required this.isOnStart,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedAlign(
-      curve: Curves.easeInOutCirc,
-      alignment: isOnStart ? Alignment.centerLeft : Alignment.centerRight,
-      duration: Duration(milliseconds: ((1000 / car.maxSpeed) * 1000).toInt()),
-      child: Image.asset(
-        car.imagePath,
-        width: 200,
-      ),
+      body: const Placeholder(),
     );
   }
 }
